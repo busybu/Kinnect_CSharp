e539e9eb6698e9d635f096bfe9f324710e4a2549
ThiagoDSMarcelino <thiagodsmarcelino@gmail.com>
2022-12-20 13:35:43 -0300

    Finish Epoch

--- NeuralNetwork.cs
+++ NeuralNetwork.cs
@@ -15,13 +15,22 @@ public class NeuralNetwork
 
     public Layer[] Layers { get; private set; }
 
-    public float[] N(params float[] inputs)
+    public float[] Output(params float[] inputs)
     {
         for (int i = 0; i < this.Layers.Length; i++)
             inputs = this.Layers[i].Output(inputs);
 
         return inputs;
     }
+
+    public float[] OutputBefore(int before, params float[] inputs)
+    {
+        for (int i = 0; i < this.Layers.Length - before; i++)
+            inputs = this.Layers[i].Output(inputs);
+
+        return inputs;
+    }
+
     public (int, float) Choose(params float[] inputs)
     {
         for (int i = 0; i < this.Layers.Length; i++)
@@ -37,10 +46,10 @@ public class NeuralNetwork
         float E = 0;
         for (int i = 0; i < X.Length; i++)
         {
-            var nx = N(X[i]); 
-            for (int j = 0; j < X[0].Length; j++)
+            var outputX = this.Output(X[i]);
+            for (int j = 0; j < outputX.Length; j++)
             {
-                float value = nx[j] - Y[i][j];
+                float value = outputX[j] - Y[i][j];
                 value = value * value;
                 E += value;
             }
@@ -48,8 +57,69 @@ public class NeuralNetwork
         return E / (0.5f * X.Length * X[0].Length);
     }
 
-    public float Fit()
+    public void Fit(float[][] X, float[][] Y, int epochs = 100, float eta = 0.05f)
     {
-        return 1f;
+        for (int i = 0; i < epochs; i++)
+            this.epoch(X, Y, eta);
+    }
+
+    private void epoch(float[][] X, float[][] Y, float eta)
+    {
+        for (int l = 0; l < this.Layers.Length; l++)
+        {
+            for (int n = 0; n < this.Layers[l].Neurons.Length; n++)
+            {
+                var neuron = this.Layers[l].Neurons[n];
+
+                var error = Score(X, Y);
+                neuron.B += eta;
+                var newError = Score(X, Y);
+                if (newError > error)
+                    neuron.B -= 2 * eta;
+                error = newError;
+
+                for (int w = 0; w < neuron.Ws.Length; w++)
+                {
+                    neuron.Ws[w] += eta;
+                    newError = Score(X, Y);
+                    if (newError > error)
+                        neuron.Ws[w] -= 2 * eta;
+                    error = newError;
+                }
+            }
+        }
+    }
+
+    private void f_epoch(float[][] X, float[][] Y, float eta = 0.01f)
+    {
+        Layer lastLayer = this.Layers.Last();
+        float[] delta = new float[lastLayer.Neurons.Length];
+        for (int i = 0; i < lastLayer.Neurons.Length; i++)
+        {
+            for (int j = 0; j < X.Length; j++)
+            {
+                float[] Z = this.Output(X[j]);
+                delta[i] += (Z[i] - Y[j][i]) * Z[i] * (1 - Z[i]);
+            }
+
+            Neuron neuron = lastLayer.Neurons[i];
+            neuron.B -= eta * delta[i];
+        }
+
+        int wcount = lastLayer.Neurons.First().Ws.Length;
+        for (int i = 0; i < lastLayer.Neurons.Length; i++)
+        {
+            Neuron neuron = lastLayer.Neurons[i];
+            for (int w = 0; w < wcount; w++)
+            {
+                float wderiv = 0;
+                for (int x = 0; x < X.Length; x++)
+                {
+                    float[] Z = this.OutputBefore(1, X[x]);
+                    wderiv += Z[w] * delta[i];
+                }
+                neuron.Ws[w] -= eta * wderiv;
+            }
+        }
     }
 }
