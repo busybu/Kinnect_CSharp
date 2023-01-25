6c577692418a9d2b0660c4a1b9009183a2147626
BrnCalisario <bruno145224@gmail.com>
2023-01-25 13:26:04 -0300

    openhand completo e variaveis declaradas no program

--- mean/Program.cs
+++ mean/Program.cs
@@ -14,7 +14,13 @@ form.WindowState = FormWindowState.Maximized;
 form.FormBorderStyle = FormBorderStyle.None;
 
 int N = 5;
+int M = N;
 float treshold = 0.15f;
+
+//Parametros de calibração da identificação do estado da mão
+int handthreshold = 2900;
+int area = 40;
+double handvalue = 0;
 
 float param = 0;
 float bgParam = 0;
@@ -83,7 +89,7 @@ tm.Tick += (o, e) =>
     }
 
     var center = hand.GetCenterPixel(bin);
-    var open = hand.IsOpenHand(bin);
+    var open = (hand.BetterOpenHand(bin, handthreshold, area)).Item1;
 
     // Pen pen = new Pen(Color.Red, 2);
     g.Clear(Color.White);
@@ -145,9 +151,11 @@ form.KeyDown += (o, e) =>
 {
     if (e.KeyCode == Keys.Space)
     {
+        N = M;
         var img = (Bitmap)crr.Clone();
         bgParam = Equalization.GetParam(img);
         bg = Blur.QuickParallelBlurGray(img, N);
+        N = 0;
     }
 
      if (e.KeyCode == Keys.O)
@@ -164,8 +172,34 @@ form.KeyDown += (o, e) =>
     }
     else if (e.KeyCode == Keys.L)
     {
-        N = N > 0 ? N-- : N;
+        if (N > 0)
+            {
+                N--;
+            }
     }
+    else if (e.KeyCode == Keys.W)
+    {
+        handthreshold += 50;
+    }
+    else if (e.KeyCode == Keys.S)
+    {
+        if (handthreshold > 0)
+        {
+            handthreshold -= 50;
+        }
+    }
+    else if (e.KeyCode == Keys.E)
+    {
+        area++;
+    }
+    else if (e.KeyCode == Keys.D)
+    {
+        if (area > 0)
+        {
+            area--;
+        }
+    }
+    
 };
 
 Application.Run(form);
