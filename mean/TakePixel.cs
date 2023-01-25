b9af906af583ff891b66354252fe88ec5bf199cc
Gabrielle Fialkoski <77334129+busybu@users.noreply.github.com>
2023-01-25 15:56:56 -0300

    TakePixel corrigido

--- mean/TakePixel.cs
+++ mean/TakePixel.cs
@@ -1,9 +1,9 @@
 public class TakePixel
 {
-    public bool Detect(Point point, Point last, int Calibration){
+    public bool Detect(Point point, Point last, int Min, int Max){
         var DistanciaDoUltimo = Math.Sqrt(Math.Pow(Math.Abs(last.X - point.X),2) + Math.Pow(Math.Abs(last.Y - point.Y),2));
         
-        if (DistanciaDoUltimo > Calibration)
+        if (DistanciaDoUltimo > Min && DistanciaDoUltimo > Max)
         {
             return true;
         }
